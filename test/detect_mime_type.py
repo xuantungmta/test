62158641d165b87be5bef3112e48919ccb3e9a3a
xuantungmta <xuantungmta@gmail.com>
2022-12-11 14:24:24 +0700

    ok

--- test/detect_mime_type.py
+++ test/detect_mime_type.py
@@ -1,4 +1,16 @@
 import mimetypes
 
-mt = mimetypes.guess_type("./shell.php")
-print(mt)
+header = bytes.fromhex("89504E470D0A1A0A0000000D49484452")
+file_name = "./shell.php"
+
+
+def change_hex():
+    with open(file_name, "rb") as f:
+        content = f.read()
+
+    with open(file_name, "wb") as f:
+        f.write(header)
+        f.write(content)
+
+
+change_hex()
