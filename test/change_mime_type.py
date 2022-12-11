981428467e0dd5e1db22c0ea88cccba266256f49
xuantungmta <xuantungmta@gmail.com>
2022-12-11 14:41:34 +0700

    ok

--- test/change_mime_type.py
+++ test/change_mime_type.py
@@ -2,13 +2,14 @@ import mimetypes
 
 header = bytes.fromhex("89504E470D0A1A0A0000000D49484452")
 file_name = "./shell.php"
+output_name = "./fake-shell.php"
 
 
 def change_hex():
     with open(file_name, "rb") as f:
         content = f.read()
 
-    with open(file_name, "wb") as f:
+    with open(output_name, "wb") as f:
         f.write(header)
         f.write(content)
 
